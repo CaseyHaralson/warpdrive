85680bbc17deb1db1dc8489a832e693ffc27b31f
DESKTOP-M3EH82I\Casey <casey.haralson@gmail.com>
2022-10-03 17:28:46 -0600

    updated home links and descriptions. app may or may not end up as described

--- nextjs-app/pages/index.tsx
+++ nextjs-app/pages/index.tsx
@@ -22,41 +22,32 @@ function Home() {
               Even 
               <a href='https://www.youtube.com/watch?v=VcKp7Iiepsg'
                 target='_blank'
-                rel='noopener noreferrer'>
-                  Zefram Cochrane
-              </a> 
+                rel='noopener noreferrer'>Zefram Cochrane</a> 
               would enjoy himself!
             </small>
           </div>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>SpaceX Server Side &rarr;</h2>
+            <p>Using the SpaceX GraphQL api and rendering the content server side.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Apollo Server &rarr;</h2>
+            <p>Some sort of GraphQL server made from open data?</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>Python Flask Api &rarr;</h2>
+            <p>Probably just a simple api communication from the Next server to the Flask server.</p>
           </a>
         </div>
       </main>
